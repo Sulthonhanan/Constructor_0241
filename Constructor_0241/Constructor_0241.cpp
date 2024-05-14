0790d9acf986e4a95cb00eb702e8af131a6983dc
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-14 14:28:26 +0700

    Add project files.

---
+++ Constructor_0241/Constructor_0241.cpp
@@ -0,0 +1,17 @@
+// Constructor_0241.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+
+
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
