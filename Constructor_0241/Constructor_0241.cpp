ecf6eadbe22ce70562441978ec4762ba01be06b2
Sulthonhanan <hananakhnaf31@gmail.com>
2024-05-14 14:54:00 +0700

    pengeluaran hasil yang di dapat

--- Constructor_0241/Constructor_0241.cpp
+++ Constructor_0241/Constructor_0241.cpp
@@ -48,12 +48,6 @@ int main()
     mahasiswa mhs3("Andi Kurniawan");
     mahasiswa mhs4(" Joko Purbo");
 
-    mhs1.printALL();
-    mhs2.printALL();
-    mhs3.printALL();
-    mhs4.printALL();
-    return 0;
-
-
+   
 }
 
